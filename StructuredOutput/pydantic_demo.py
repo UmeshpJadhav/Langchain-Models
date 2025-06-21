845b735eefaf3441340e4a68e2ded103b60ccdb3
UmeshpJadhav <Umesh2005jadhav@gmail.com>
2025-06-21 21:31:02 +0530

    pydantic

--- StructuredOutput/pydantic_demo.py
+++ StructuredOutput/pydantic_demo.py
@@ -1,12 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 
 class Student(BaseModel):
 
-    name: str
+    name: str = "Umesh"
+    age : Optional[int] = None
+    email : EmailStr = "umesh@gmail.com"
+    cgpa : float = Field(gt=0, lt=10,default=5, description="CGPA must be between 0 and 10")
 
-new_student = {"name": "John Doe"}
+new_student = { "cgpa": 1 }
 
 Student = Student(**new_student)
 
-print(Student)
+student_dict = dict(Student)
 
+print(student_dict ['age'])
+
+student_json = Student.model_dump_json()
